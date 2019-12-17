1d637f2666b50c18c3fe4a1692b5317ca68dc150
mykola-bartkiv <bartkivmm93@gmail.com>
2019-12-17 23:26:24 +0200

    upload

---
+++ footer.php
@@ -0,0 +1,22 @@
+</div>
+<footer>
+<a href="#" class="burger desctop_hide"><span class="burger-icon "></span></a>
+<div class="mobile_menu_container desctop_hide cfx">
+    <nav class="mobile_nav">
+        <ul class="level_a">
+            <li><a href="">Home</a></li>
+            <li class="menu-item-has-children ">
+            	<a href="">About</a>
+	            <ul class="sub-menu">
+	                <li><a href="">Sub Menu About</a></li>
+	            </ul>
+            </li>            
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Company</a></li>
+        </ul>
+    </nav>
+</div>
+</footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>The Block</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="#">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="scss/main.css" rel="stylesheet" type="text/css">
+    <link href="scss/components/100_lib.css" rel="stylesheet" type="text/css">
+    <link href="https://file.myfontastic.com/UCh5HgwSYvMAtExGhpHLqd/icons.css" rel="stylesheet">
+    <meta name="theme-color" content="#000">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="js/lib.js"></script>
+    <script src="js/init.js"></script>
+</head>
+<body>
+<div id="wrap">
+<header class="header">
+    <div class="row end-center">
+    <nav class="main_nav  mobile_hide">
+        <ul class="level_a">
+            <li><a href="">Home</a></li>
+            <li class="menu-item-has-children "><a href="">About</a>
+            <ul class="sub-menu">
+                <li><a href="">Sub Menu About</a></li>
+            </ul>
+            </li>            
+            <li><a href="">Portfolio</a></li>
+            <li><a href="">Contact Us</a></li>
+            <li><a href="">Company</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
