906af7a3f28b05797b2647c5691ff3f37ee348d4
Mykola Bartkiv <bartkivmm93@gmail.com>
2020-02-24 12:43:44 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,34 +1,24 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>The Block</title>
+    <title>Block</title>
     <meta charset="UTF-8">
     <meta name="description" content="#">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="scss/main.css" rel="stylesheet" type="text/css">
-    <link href="scss/components/100_lib.css" rel="stylesheet" type="text/css">
-    <link href="https://file.myfontastic.com/UCh5HgwSYvMAtExGhpHLqd/icons.css" rel="stylesheet">
-    <meta name="theme-color" content="#000">
+    <link rel="apple-touch-icon" sizes="152x152" href="img/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
+    <link rel="manifest" href="img/favicon/site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <link href="style/libs.css" rel="stylesheet" type="text/css">
+    <link href="style/style.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/lib.js"></script>
-    <script src="js/init.js"></script>
+    <script src="js/logic.js"></script>
 </head>
 <body>
-<div id="wrap">
-<header class="header">
-    <div class="row end-center">
-    <nav class="main_nav  mobile_hide">
-        <ul class="level_a">
-            <li><a href="">Home</a></li>
-            <li class="menu-item-has-children "><a href="">About</a>
-            <ul class="sub-menu">
-                <li><a href="">Sub Menu About</a></li>
-            </ul>
-            </li>            
-            <li><a href="">Portfolio</a></li>
-            <li><a href="">Contact Us</a></li>
-            <li><a href="">Company</a></li>
-        </ul>
-    </nav>
-</div>
-</header>
+<div class="main">
+    <header>
+
+    </header>
