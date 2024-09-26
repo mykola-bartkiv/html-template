de5e83772a494a3945f3d49b100c6d252721b2fd
Mykola Bartkiv <bartkivmm93@gmail.com>
2024-09-26 11:44:26 +0300

    Update header.php

--- header.php
+++ header.php
@@ -15,10 +15,60 @@
     <link href="style/style.css" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="js/lib.js"></script>
-    <script src="js/logic.js"></script>
+    <script src="js/init.js"></script>
 </head>
 <body>
 <div class="main">
     <header>
-
+             <nav class="main-menu">
+                <ul id="menu-main-menu">
+                    <li class="menu-item current-menu-item">
+                        <a href="#" aria-current="page">Residences</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a href="#">Test</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item menu-item-has-children">
+                        <a href="#">Amenities</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Location</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Gallery</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Team</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="menu-burger"></div>
+            <nav class="mobile-main-menu">
+                <ul id="menu-mobile-main-menu">
+                    <li class="menu-item current-menu-item">
+                        <a href="#" aria-current="page">Residences</a>
+                        <ul class="sub-menu">
+                            <li class="menu-item"><a href="#">Test</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item menu-item-has-children">
+                        <a href="#">Amenities</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Location</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Gallery</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Team</a>
+                    </li>
+                    <li class="menu-item">
+                        <a href="#">Contact</a>
+                    </li>
+                </ul>
+            </nav>
     </header>
